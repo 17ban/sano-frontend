25a21a07b8848f51c1af755bb12fd2f9b89022aa
17BAN <17ban.1024@gmail.com>
2021-06-13 15:33:02 +0800

    refactor: node api

--- src/store/nodes-cache.ts
+++ src/store/nodes-cache.ts
@@ -11,56 +11,65 @@ import {
   getNodeBundle as getNodeBundleApi,
 } from '../api/node'
 
-export const sanoNodeMapRef = ref<SanoNodeMap>({})
+export const cachedSanoNodeMap = ref<SanoNodeMap>({})
 
-async function _cacheNode<NID extends Nid>(nid: NID) {
+export async function cacheNode<NID extends Nid>(nid: NID): Promise<SanoNode | undefined> {
   const res = await getNodeApi(nid)
   if (!res.ok) {
     console.error('请求失败😥', res)
     return
   }
   const node = await res.json()
-  sanoNodeMapRef.value[node.nid] = node
+  cachedSanoNodeMap.value[node.nid] = node
   return node
 }
 
-async function _cacheNodes<NID extends Nid>(nids: NID[]) {
+export async function cacheNodes<NID extends Nid>(nids: NID[]): Promise<SanoNodeMap | undefined> {
   const res = await getNodesApi(nids)
   if (!res.ok) {
     console.error('请求失败😥', res)
     return
   }
   const nodes = await res.json()
-  Object.assign(sanoNodeMapRef.value, nodes)
+  Object.assign(cachedSanoNodeMap.value, nodes)
   return nodes
 }
 
-export async function cacheNodeBundle(nid: Nid) {
+export async function cacheNodeBundle<NID extends Nid>(nid: NID): Promise<SanoNodeMap | undefined> {
   const res = await getNodeBundleApi(nid)
   if (!res.ok) {
     console.error('请求失败😥', res)
     return
   }
   const nodes = await res.json()
-  Object.assign(sanoNodeMapRef.value, nodes)
+  Object.assign(cachedSanoNodeMap.value, nodes)
   return nodes
 }
 
-export async function getNode<NID extends Nid>(nid: NID): Promise<SanoNode | undefined> {
-  return <SanoNode | undefined>sanoNodeMapRef.value[nid] || await _cacheNode(nid)
+export async function getNode<NID extends Nid>(nid: NID) {
+  return cachedSanoNodeMap.value[nid] || await cacheNode(nid)
 }
 
-export async function getNodes<NID extends Nid>(nids: NID[]): Promise<SanoNodeMap> {
+export async function getNodes<NID extends Nid>(nids: NID[]) {
   const nodes: SanoNodeMap = { }
   const getList: Nid[] = []
   for (const nid of nids) {
-    if (sanoNodeMapRef.value[nid])
-      nodes[nid] = sanoNodeMapRef.value[nid]
+    if (cachedSanoNodeMap.value[nid])
+      nodes[nid] = cachedSanoNodeMap.value[nid]
     else
       getList.push(nid)
   }
   if (getList.length > 0)
-    Object.assign(nodes, await _cacheNodes(getList))
+    Object.assign(nodes, await cacheNodes(getList))
 
   return nodes
+}
+
+export async function getNodeBundle<NID extends Nid>(nid: NID) {
+  const node = await getNode(nid)
+  if (!node)
+    return
+  const nodeMap = await getNodes(node.children)
+  nodeMap[nid] = node
+  return nodeMap
 }
